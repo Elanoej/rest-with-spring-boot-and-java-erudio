41eb6489fb5436fabada7b102ca40f4a4cba1204
Elano Marcena <elanomarcena@gmail.com>
2023-07-26 15:45:08 -0300

    11_MigrationsWithFlyWay

---
+++ 11_MigrationsWithFlyWay/rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,8 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1,'São paulo','Ayrton','Male','Senna'),
+    (2,'Anchiano - Italy','Leonardo','Male','da Vinci'),
+    (4,'Porbandar - India','Indira','Female','Gandhi'),
+    (5,'Porbandar - India','Mahatma','Male','Gandhi'),
+    (7,'Kentuck - US','Muhammad','Male','Ali'),
+    (9,'Mvezo - Sout Africa','Nelson','Male','Mvezo'),
+    (10,'Smiljan - Croatia','Nikola','Male','Tesla');
